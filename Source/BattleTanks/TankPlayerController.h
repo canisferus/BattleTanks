d13e0e3cae43b9302009c8d27d24e7fd4740d212
Lukasz Wesiora <canis.ferus@gmail.com>
2018-12-17 15:58:48 +0000

    Lesson 127 start with c++

---
+++ Source/BattleTanks/TankPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Public/Tank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+		ATank*GetControlledTank()const;
+};
